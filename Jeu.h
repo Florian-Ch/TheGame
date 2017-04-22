59f4048daa18f13426c248ae2c0597c336c4a8c3
Florian-Ch <gassenbube@gmail.com>
2017-04-22 15:11:17 +0200

    Ajout des commentaires pour doxygen
    Normalement tout est fonctionnel pour la doc

--- Jeu.h
+++ Jeu.h
@@ -1,22 +1,64 @@
+/*!
+\file Jeu.h
+\brief Header de la classe Jeu
+\author Florian
+\date 22 avril 2017
+\version 1
+*/
+
+
+/*! \class Jeux Jeu.h
+    \brief classe Jeux
+
+    Gère le fonctionnement du jeu en général
+*/
 class Jeux{
 private:	
+	/*!
+		\enum Etat
+		\brief états possibles du jeu
+     	*/
 	enum Etat {
-		e_Initialisation,
-		e_Exploration,
-		e_Combat,
-		e_FinDuJeu
+		e_Initialisation,	/*!< Phase d'initialisation*/
+		e_Exploration,		/*!< Phase de déplacement sur la map*/
+		e_Combat,		/*!< Phase de combat*/
+		e_FinDuJeu		/*!< Phase de fin de partie*/
 	};
-	Etat etatJeux;
-	Joueur* heros;
-	int difficulty;
-	Map carte;
-	bool end;
+	Etat etatJeux;	//!< Etat actuel du jeu
+	Joueur* heros;	//!< Héros incarné par le joueur
+	int difficulty;	//!< Difficulté du jeu
+	Map carte;	//!< Map qui sera utilisée pour la partie
+	bool end;	//!< Détecte la fin du jeu
 public:
+/*!
+\brief Constructeur
+	Passe l'état du jeu à Initialisation
+	Initialise la fin à false
+*/
 	Jeux();
+/*!
+\brief Permet au joueur de choisir son héros parmi ceux créés par le client
+*/
 	void m_getJoueur();
+/*!
+\brief Permet au joueur de choisir la difficulté
+*/
 	void m_selectDifficulty();
+/*!
+\brief Choisi la map de façon aléatoire parmi celles de la difficulté donnée
+*/
 	void m_getMap();
+/*!
+\brief Actualise les phases de jeu
+*/
 	void m_update();
+/*!
+\brief Fonction pour transmettre au main.cpp la fin du jeu
+\return vrai ou faux pour terminer la partie dans le main
+*/
 	bool m_getEtat();
+/*!
+\brief Destructeur
+*/
 	~Jeux();
 };
